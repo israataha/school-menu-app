00d32f87c187c0afda542831ca673a2cee5433c5
israataha <israataha@gmail.com>
2025-08-20 11:32:08 -0500

    Configure ESLint & Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,29 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const eslintPluginReactNative = require('eslint-plugin-react-native');
+const eslintPluginSimpleImportSort = require('eslint-plugin-simple-import-sort');
+const eslintPluginUnusedImports = require('eslint-plugin-unused-imports');
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*'],
+    plugins: {
+      'react-native': eslintPluginReactNative,
+      'simple-import-sort': eslintPluginSimpleImportSort,
+      'unused-imports': eslintPluginUnusedImports,
+    },
+    rules: {
+      curly: ['error', 'multi'],
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react-native/no-unused-styles': 2,
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'unused-imports/no-unused-imports': 'error',
+    },
   },
 ]);
